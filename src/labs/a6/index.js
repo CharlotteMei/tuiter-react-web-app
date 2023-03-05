0ff516ccfb188bf7f4933956d1b3555c01267893
Charlotte <charlottemay.234@gmail.com>
2023-03-05 11:16:12 -0800

    before todo

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,12 +1,17 @@
-import {Link}
-  from "react-router-dom";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
 
 function Assignment6() {
- return(
-  <div>
-   <h1>Assignment 6</h1>
-  </div>
- )
+ return (
+   <div>
+     <h1>Assignment 6</h1>
+     <ConditionalOutput/>
+     <Styles/>
+     <Classes/>
+   </div>
+ );
 }
 export default Assignment6;
+
 
