9a45d8b0aa4f2fe3820ce36bfd98c038fdd88475
Charlotte <charlottemay.234@gmail.com>
2023-04-02 19:43:53 -0700

    adding things to react app for dislikes

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -13,33 +13,45 @@ const TuitStats = ({ post }) => {
 
     // let likeSimbol = post.liked ? <i class="fa-solid fa-heart"></i> : <i class="fa-regular fa-heart"></i>
     let tuit = post
-    const [likeCount, setLikeCount] = useState(tuit.likes);
     return (
         <div>
             <div class="row">
-                <div class="col-3">
+                <div class="col-2">
                     <i class="fa-regular fa-comment"></i>
                     {tuit.replies}
                 </div>
-                <div class="col-3">
+                <div class="col-2">
                     <i class="fa-regular fa-handshake"></i>
                     {tuit.retuits}
                 </div>
-                <div class="col-3">
+                <div class="col-2">
                     <i
                         className="bi bi-heart-fill me-2 text-danger"
                         onClick={
                             () => {
                                 dispatch(updateTuitThunk({
-                                ...tuit,
-                                likes: likeCount+1
-                            }))
-                                setLikeCount(likeCount+1)
+                                    ...tuit,
+                                    likes: tuit.likes + 1
+                                }))
+
                             }} ></i>
-                    {likeCount}
+                    {tuit.likes}
 
                 </div>
-                <div class="col-3">
+                <div class="col-2">
+                    <i class="fa-solid fa-thumbs-down"
+                        onClick={
+                            () => {
+                                dispatch(updateTuitThunk({
+                                    ...tuit,
+                                    dislikes: tuit.dislikes + 1
+                                }))
+
+                            }} ></i>
+                    {tuit.dislikes}
+
+                </div>
+                <div class="col-auto">
                     <i class="fa-regular fa-share-from-square"></i>
                 </div>
 
--- src/tuiter/tuits/tuits-reducer.js
+++ src/tuiter/tuits/tuits-reducer.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 // import tuits from './tuits.json';
-import { findTuitsThunk, deleteTuitThunk, createTuitThunk }
+import { findTuitsThunk, deleteTuitThunk, createTuitThunk, updateTuitThunk }
   from "../../services/tuits-thunks";
 
 const initialState = {
@@ -56,6 +56,17 @@ const tuitsSlice = createSlice({
         state.loading = false
         state.tuits.push(payload)
       },
+
+    [updateTuitThunk.fulfilled]:
+      (state, { payload }) => {
+        state.loading = false
+        const tuitNdx = state.tuits
+          .findIndex((t) => t._id === payload._id)
+        state.tuits[tuitNdx] = {
+          ...state.tuits[tuitNdx],
+          ...payload
+        }
+      }
   },
 
   // old reducers not in use anymore
